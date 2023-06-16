5fd8685e35ff82d4f3426b9a821d9d7b71c67f42
Daniel Suarez <suarez.daniel101@gmail.com>
2023-06-16 01:36:45 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,15 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie) => (
+          <div>
+          <h1>Name: {movie.title}</h1>
+          <p>Time: {movie.time}</p>
+          <p>Genres: {movie.genres.map((genre) => (
+            <li>{genre}</li>))}</p>
+          </div> 
+        ))}
     </div>
   );
 };
